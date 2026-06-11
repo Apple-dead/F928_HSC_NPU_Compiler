1a83dda46b54f1eac2e22e86f2bc7cb093e2c19f
Apple <601957417@qq.com>
2026-06-12 00:15:54 +0800

    temp

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ MODEL_NAME="yolov2_fixed.pth"
 
 #========PARAMETERS========
 BIAS_1_LENGTH=256
-
+BIAS_1_MOVE=512
 #===========DATA=============
 IMAGE_PATH="./data/image.jpg"
 WEIGHT_1_PATH="./data/model_params/layer1_0_weight.txt"
@@ -33,7 +33,7 @@ python ./python/image_to_bram_coe.py "$IMAGE_PATH" "$IMAGE_COE_PATH"
 python ./python/weight_to_bram_coe.py "$WEIGHT_1_PATH" "$WEIGHT_1_COE_PATH"
 
 #bias to coe
-python ./python/bias_to_bram.py -length "$BIAS_1_LENGTH" "$BIAS_1_PATH" "$BIAS_1_COE_PATH"
+python ./python/bias_to_bram.py -length "$BIAS_1_LENGTH" -move "$BIAS_1_MOVE" "$BIAS_1_PATH" "$BIAS_1_COE_PATH"
 
 #instr to coe
 python ./python/instr_txt_to_bram_coe.py "$INSTR_PATH" "$INSTR_COE_PATH"
